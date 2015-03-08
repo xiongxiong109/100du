300c822132baf3c8e0e5b09d8d4652f7e46fa69b
xiongxiong109 <444122662@qq.com>
2015-03-08 11:35:07 +0800

    completed all parts v1.0

--- js/main.js
+++ js/main.js
@@ -94,7 +94,7 @@
 				}
 				$("#updateUl").stop().animate({"top" : -oLis.height()*iCur},400,'swing');
 				//修复快速点击时ul消失的bug
-				if( Math.abs(oTop) >=$("#updateUl").height() ){
+				if( Math.abs(oTop) >$("#updateUl").height() ){
 					$("#updateUl").css("top",0);
 					iCur=1;
 				}
@@ -133,5 +133,122 @@
 				},2000);
 			});
 		})();
+
+		//tab切换模块
+		(function(){
+			$.tab($("#tab1 li"),'click',$("#tCon1 .tab-content"));
+			$.tab($("#tab2 li"),'click',$("#tab2 .tab-content"));
+			$.tab($("#knowTab li"),'mouseover',$("#knowWrap .i-know"));
+			$.tab($("#sideTabUl li"),'mouseover',$(".sideTabUl"));
+		})();
+
+		//精彩推荐轮播图
+		(function(){
+			var oTriggers=$(".slider-wrap .tab-slider").find("li");
+			var oShows=$(".slider-wrap .big-slider").find("li");
+			var oTexts=$(".slider-wrap .slider-text").find('li');
+			var iCur=oShows.filter('[class=active]').index();
+			oTriggers.on('click',function(){
+				oTriggers.removeClass('active');
+				$(this).addClass('active');
+				oShows.eq(iCur).stop().fadeOut();
+				oTexts.eq(iCur).hide();
+				oShows.eq($(this).index()).stop().fadeIn();
+				oTexts.eq($(this).index()).show();
+				iCur=$(this).index();
+			});
+			var timer=null;
+			var delay=2000;
+			clearInterval(timer);
+			timer=setInterval(function(){
+				autoSlider();
+			},delay);
+			$(".slider-wrap").hover(function(){
+				clearInterval(timer);
+			},function(){
+				timer=setInterval(function(){
+					autoSlider();
+				},delay);
+			});
+			function autoSlider(){
+				iCur++;
+				if(iCur>=oTriggers.length){
+					iCur=0;
+				}
+				oTriggers.removeClass('active');
+				oTriggers.eq(iCur).addClass('active');
+				if(iCur-1<0){
+					oShows.eq(oTriggers.length-1).stop().fadeOut();
+					oTexts.eq(oTriggers.length-1).hide();
+				}else{
+					oShows.eq(iCur-1).stop().fadeOut();
+					oTexts.eq(iCur-1).hide();
+				}
+				oShows.eq(iCur).stop().fadeIn();
+				oTexts.eq(iCur).show();
+			}
+		})();
+
+		//bbs
+		(function(){
+			$(".bbs-list").find("li").hover(function(){
+				$(this).find(".normal-list").hide();
+				$(this).find('.hover-list').show();
+			},function(){
+				$(this).find(".normal-list").show();
+				$(this).find('.hover-list').hide();
+			});
+		})();
+
+		//红人烧客
+		(function(){
+			$(".hot-img-wrap").find(".box").hover(function(){
+				$(this).find('.mark').stop().fadeIn();
+			},function(){
+				$(this).find('.mark').stop().fadeOut();
+			});
+		})();
+
+		//日历部分
+		(function(){
+			var data=[
+				{"url":"img/pizza.jpg"},
+				{"url":"img/show.jpg"}
+			]
+			var oTodos=$(".clen .todo");
+			var oHover=$(".hover-wrap");
+			oTodos.on({
+				'mouseenter':function(){
+					//模拟ajax
+					if($(this).html()=='17'){
+						oHover.find(".big").attr('src',data[0].url);
+					}else if($(this).html()=='21'){
+						oHover.find(".big").attr('src',data[1].url);
+					}
+					oHover.show();
+					oHover.css({
+						"left":$(this).offset().left+$(this).width()+$(".hover-info .arrow").width(),
+						"top":$(this).offset().top+$(this).height()/2-oHover.height()/2
+					});
+				},
+				'mouseleave':function(){
+					oHover.hide();
+				}
+			})
+		})();
 	});
+//tab切换
+$.extend({
+	'tab':function(trig,ev,res){
+		trig.on( ev,function(){
+			var oArrow=trig.find('*').filter('[class*=arrow]');
+			oArrow.removeClass('a-down-red').addClass('a-down-grey');
+			trig.removeClass('active').addClass('gradient');
+			res.hide();
+			res.eq($(this).index()).show();
+			$(this).removeClass('gradient').addClass('active');
+			oArrow.eq($(this).index()).removeClass('a-down-grey').addClass('a-down-red');
+		} );
+	}
+});
 })(jQuery);
